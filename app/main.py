7b8b4a5aa0c66ef3ca3e2e2d40ad76dafb664e56
Stephen <stephendpurdue@gmail.com>
2026-09-17 17:39:08 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,36 @@
 import sys
+import shutil
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush() # Forces buffered text to the terminal
 
-        command = input()
-
-        # Commands 
-        if command == "exit".lower():
+        try:
+            cmd = input()
+        except EOFError:
             break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command [5:] in ["type", "echo", "exit"]:
-            print(f"{command[5:]} is a shell builtin")
-        else:
-            print(f"{command.strip('type ')}: not found")
+    
+        if cmd == "exit":
+            break
+        elif cmd.startswith("echo "):
+            print(cmd[5:])
 
-        
+        elif cmd.startswith("type "):
+            target = cmd[5:]
+            if target in ["type", "echo", "exit"]:
+                print(f"{target} is a shell builtin")
+            elif path := shutil.which(target):
+                print(f"{target} is {path}")
+            else:
+                print(f"{target}: not found")
+
+        elif path := shutil.which(cmd):
+            print(f"{cmd} is {path}")
+
+        else:
+            print(f"{cmd}: not found")
 
 
 if __name__ == "__main__":
